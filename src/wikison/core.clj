4164a32fbf0a3511d4676e3337047e707b6796fa
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-11-21 14:58:09 +0000

    renamed function because i could not remember its name

--- src/wikison/core.clj
+++ src/wikison/core.clj
@@ -64,7 +64,8 @@
                        :pages)]
     (-> resp-dic keys first resp-dic)))
 
-(defn raw-article-prop
+(defn raw-article
+
   "retrieve article properties that will go into the json article. This is
   the raw result from the wiki api"
   [url]
@@ -81,7 +82,8 @@
 
 (defn simple-prop-extract
   "Extract the simple properties (ones that directly map to a property in the
-  json result) from the raw-article-prop result" 
+  json result) from the  raw-article
+  result" 
   [raw]
   (let [new-raw (cset/rename-keys raw
                                   {:fullurl :url
@@ -89,13 +91,15 @@
     (select-keys new-raw [:url :title :pageid :lang])))
 
 (defn languages-extract
-  "extract the languages from the raw-article-prop result"
+  "extract the languages from the  raw-article
+  result"
   [raw]
   (let [raw-languages (raw :langlinks)]
     {:other-langs (vec (map :lang raw-languages))}))
 
 (defn thumbnail-extract
-  "extract the thumbnail from the raw-article-prop result"
+  "extract the thumbnail from the  raw-article
+  result"
   [raw]
   {:depiction (-> raw :thumbnail :source)})
 
@@ -134,7 +138,7 @@
 (defn article 
   "return a json document built from the given url"
   [url]
-  (let [raw-result (raw-article-prop url)
+  (let [raw-result (raw-article url)
         simple (simple-prop-extract raw-result)
         lang (languages-extract raw-result)
         thumb (thumbnail-extract raw-result) 
