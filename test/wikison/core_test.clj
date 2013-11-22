f50e131a9d5dc2883e408b7acaa043d3c94bfb6e
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-11-22 19:57:43 +0000

    implementing robust tests

--- test/wikison/core_test.clj
+++ test/wikison/core_test.clj
@@ -1,6 +1,7 @@
 (ns wikison.core-test
   (:require [clojure.test :refer :all]
-            [wikison.core :refer :all]))
+            [wikison.core :refer :all]
+            [instaparse.core :as insta]))
 
 ; an example api call result
 (def api-result {:langlinks [
@@ -46,6 +47,25 @@
               {:title "In music" :text "Whistler and particularly"}]
    })
 
+(def testfiles
+  ["en-test-1.txt"
+   "es-test-1.txt"
+   "fr-test-1.txt"
+   "it-test-1.txt"
+   "ja-test-1.txt"
+   "nl-test-1.txt"
+   "pt-test-1.txt"
+   "ru-test-1.txt"
+   "edge-test-1.txt"
+   "edge-test-2.txt"])
+
+(defn load-tests
+  [files]
+  (let [path "./test/wikison/extracts/"]
+    (letfn [(load-test [file] {(keyword file) (slurp (str path file))})]
+      (apply merge (map load-test files)))))
+
+(def test-cases (load-tests testfiles))
 
 ; test-extract tests.
 (deftest parser-simple-1
@@ -84,7 +104,7 @@
           ou (text-eval in)]
       (is (= ex ou)))))
 
-; is that unreadable or what?
+; thats unreadable.
 (deftest parser-simple-5
   (testing "All levels of indentation"
     (let [in (wiki-parser (slurp "./test/wikison/extracts/simple-test-5.txt"))
@@ -104,23 +124,64 @@
           ou (text-eval in)]
       (is (= ex ou)))))
 
-(deftest parser-ru-simple-1
-  (testing "abstract only with russian char"
-    (let [in (wiki-parser (slurp "./test/wikison/extracts/ru-simple-test-1.txt"))
-          ex {:abstract "привет мир от русских коды символов!\n"}
-          ou (text-eval in)]
-      (is (= ex ou)))))
+; russian text parsing test.
+(def ru-exp-1 
+  {:abstract "(лат. Tiberius Claudius Drusus),годы — Тибе́рий Кла́вдий Неро́н \n"
+   :sections [
+              {:title " Ученый " :text "В 14 году, когда скончался Август, Клавдий был избран главой посольства к \n"}
+              {:title " Секретариат " :text "В первые годы своего правления Клавдий организовал императорский ."}]})
 
-; tougher test case.
+(deftest parse-ru-test-1
+  (testing "russian article."
+    (let [in (wiki-parser (slurp "./test/wikison/extracts/ru-test-1.txt"))
+          ex ru-exp-1
+          ou (text-eval in)]
+    (is (= ex ou)))))
+
+(def en-exp-1 
+  {:abstract "Peercoin (code: PPC), also known as PPCoin and Peer-to-Peer Coin is the first \n"
+   :sections [{:title " Transactions " :text "A peer-to-peer network handles Peercoin's transactions.\n\n" 
+               :sections [{:title " Addresses " :text "digital signatures. \n"} 
+                          {:title " Confirmations " :text "Transactions are recorded in the Peercoin blockchain.\n"}]}
+              {:title " Distinguishing features " :text "\n\n" 
+               :sections 
+               [{:title " Proof-of-stake " :text "Peercoin's major distinguishing feature is that it uses\n\n\n"} 
+                {:title " Proof-of-work " :text "SHA-256. For each 16x increase in the network, the PoW block reward is halved.\n"}]}
+              ]})
+
+(def es-exp-1
+  {:abstract "Los escarabeoideos (Scarabaeoidea) son una superfamilia de coleópteros \n" 
+   :sections [
+               {:title " Características " :text "Las características distintivas de este vasto grupo son las antenas terminadas \n"}
+               {:title " Historia natural " :text "(Rutelinae, Cetoniinae, Melolonthinae), etc. \n"}
+               {:title " Referencias "}]})
+
+(deftest parse-es-test-1
+  (testing "spanish article"
+    (let [in (wiki-parser (slurp "./test/wikison/extracts/es-test-1.txt"))
+          ex es-exp-1
+          ou (text-eval in)]
+    (is (= ex ou)))))
+
+; grammar must be non-ambiguous for all test cases. THIS IS IMPORTANT for
+; deterministic results.
+(deftest non-ambiguous
+  (testing "non-ambiguity of grammer"
+    (let [ tests (vals test-cases)]
+      (doseq [t tests]
+        (let [results (insta/parses wiki-parser t)]
+          (is (= 1 (count results)))))))) 
+
+; edge case.
 (deftest edge-test-1
   (testing "Abstract that contain code that is section-like (e.g: 1 == 1)"
     (let [in (wiki-parser (slurp "./test/wikison/extracts/edge-test-1.txt"))
           ex {:abstract "Complex\n" 
               :sections [{:title " S1 " :text "in C : 1 == 1, in clojure (= 1 1).\n"}]}
           ou (text-eval in)]
-      (is (= ex ou)))))
+    (is (= ex ou)))))
 
-; other function
+; other function.
 (deftest article-test
   (testing "json document represents the article"
     (let [in  "https://en.wikipedia.org/wiki/Whistler's_Mother"
